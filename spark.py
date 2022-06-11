2d1f01949d04524f28a2e803a314f5a36b47a40e
Jose Mata <mata649@hotmail.com>
2022-06-11 14:16:10 -0600

    Changed spark config s3n to s3a

--- spark.py
+++ spark.py
@@ -6,5 +6,5 @@ from cfg import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
 spark = SparkSession.builder.appName('ETL cultural places Argentina').getOrCreate()
 
 spark._jsc.hadoopConfiguration().set("fs.s3a.access.key", AWS_ACCESS_KEY_ID)
-spark._jsc.hadoopConfiguration().set("fs.s3n.secret.key", AWS_SECRET_ACCESS_KEY)
-spark._jsc.hadoopConfiguration().set("fs.s3n.endpoint", "s3.amazonaws.com")
+spark._jsc.hadoopConfiguration().set("fs.s3a.secret.key", AWS_SECRET_ACCESS_KEY)
+spark._jsc.hadoopConfiguration().set("fs.s3a.endpoint", "s3.amazonaws.com")
